d94b8c3515cca9e36f9cd0e44152ad6ad224ae24
D4Gj <inzadimonax@gmail.com>
2019-10-25 12:44:27 +0400

    dev ver

--- Stack/Program.cs
+++ Stack/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Stack
 {
@@ -10,6 +6,30 @@ namespace Stack
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Алгортим");
+            var stackRdy = new Model.Stack<int>();           
+            stackRdy.Push(1);
+            Console.WriteLine(stackRdy.Peek());
+            
+
+            Console.WriteLine("");
+            Console.Write("Write down elements: ");
+            Random rnd = new Random();
+            var array = new int[10];  
+            for(int i = 0; i < array.Length; ++i)
+            {
+                array[i] = rnd.Next(100);
+            }
+            foreach (var arr in array)
+            {
+                Console.Write(arr + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Array in insert sort: {0}", string.Join(" / ", InsertSort.InsertionSort(array)));
+            Console.WriteLine("Array in merge sort: {0}", string.Join(" / ", MergeSort.MergeSorting(array,0,array.Length)));
+
+            Console.ReadLine();
         }
+        
     }
 }
